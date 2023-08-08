906436974b5ed39ae18b6905ba68ea817b32c37c
Julian Beldotti <julian.beldotti@gmail.com>
2023-08-08 10:11:37 -0600

    fourth commit

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,11 +1,16 @@
 class OpenLibraryService
-  def get_url('')
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
+  def get_books(capital)
+    get_url("/search.json?q=#{capital}")
   end
 
   private
 
   def conn
-    Faraday.new()
+    Faraday.new("https://openlibrary.org")
   end
 end
---
+++ spec/services/open_library_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe OpenLibraryService do
+  describe "instance_methods" do
+    describe "#get_books" do
+    it 'returns a hash of books by keyword(capital)' do
+      country = 'portugal'
+      
+    end
+  end
+end
